ad4dc573678afb7245f6470e80413b13cdb31097
Testificate num 7 for real <testificatenum7forreal@Macintosh.local>
2023-11-08 10:24:58 -0700

    ben is cool

---
+++ Documents/Java Test/src/App.java
@@ -0,0 +1,63 @@
+import java.util.Arrays;
+
+import javax.swing.JOptionPane;
+
+import java.lang.Math;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        /*
+         * ben is ben and i am i 
+         * no more of this wot 
+         */
+
+        for (int i = 0; i <= 23; i++) {
+            System.out.println("Micheangleo is " + "cool eh mate!");
+        }
+        int benjamen = 234;
+        String wozers = "";
+        int ben = 10; System.out.println(benjamen + ben);
+        System.out.println(ben);
+        for (int j = 10; ) {
+            System.out.println("Bne will be it ehh")
+        }
+        JOptionPane.showMessageDialog(null, args, wozers, j, null);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
